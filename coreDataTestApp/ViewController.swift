7521bc64e5b6ca00a71d3ac5b7ad8b1d8f33028a
Дмитрий Алиев <dmitrii.aliev@academmedia.com>
2016-05-24 09:37:08 +0600

    рабочая аппа с core data

--- coreDataTestApp/ViewController.swift
+++ coreDataTestApp/ViewController.swift
@@ -7,17 +7,112 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var people = [NSManagedObject]()
+    
+    @IBAction func add(sender: AnyObject) {
+        let alert = UIAlertController(title: "New name",
+                                      message: "Add a new name",
+                                      preferredStyle: .Alert)
+        
+        let saveAction = UIAlertAction(title: "Save",
+                                       style: .Default) { (action: UIAlertAction!) -> Void in
+                                        
+                                        let textField = alert.textFields![0]
+                                        self.saveName(textField.text!)
+                                        self.tableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .Default) { (action: UIAlertAction!) -> Void in
+        }
+        
+        alert.addTextFieldWithConfigurationHandler {
+            (textField: UITextField!) -> Void in
+        }
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        presentViewController(alert,
+                              animated: true,
+                              completion: nil)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //1
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        let managedContext = appDelegate.managedObjectContext
+        
+        //2
+        let fetchRequest = NSFetchRequest(entityName:"Person")
+        
+        //3
+        do {
+            let fetchedResults = try managedContext.executeFetchRequest(fetchRequest)  as? [NSManagedObject]
+            if let results = fetchedResults {
+                people = results
+            } else {
+            }
+        } catch {
+            
+        }
 
+    }
+    
+    func saveName(name: String) {
+        //1
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        let managedContext = appDelegate.managedObjectContext
+        //2
+        let entity =  NSEntityDescription.entityForName("Person", inManagedObjectContext:managedContext)
+        
+        let person = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
+        //3
+        person.setValue(name, forKey: "name")
+        //4
+        do {
+            try managedContext.save()
+            people.append(person)
+        } catch let error as NSError  {
+            print("Could not save \(error), \(error.userInfo)")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        title = "\"The List\""
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+
     }
 
+    func tableView(tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return people.count
+    }
+    
+    func tableView(tableView: UITableView,
+                   cellForRowAtIndexPath
+        indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell") as UITableViewCell!
+        
+        let person = people[indexPath.row]
+        cell.textLabel!.text = person.valueForKey("name") as? String
+        
+        return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
